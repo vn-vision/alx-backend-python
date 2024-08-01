10b334f385eb7cd09007d754b6815e036460a16a
kZilla <kzilla@vn-z.zilla>
2024-08-01 16:28:17 +0300

    python - backend dev: mind documentation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-
+'''Task 7's module.
 '''
-to_kv - takes a string k and int or float v
-@k: string
-@v: int or float
-Return - tuple (str, int|float)
-'''
-
-from typing import Tuple, Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
-    ''' return a tuple of string and float '''
-
-    return (k, float(v ** 2))
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
+    return (k, float(v**2))
